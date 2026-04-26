900e317ac458054009f62ed33df7884c932e6dbe
nhungdn <nhungdoanbg191@gmail.com>
2026-04-26 16:50:18 +0700

    fix lỗi ô chọn tháng năm không hiển thị tháng, năm đang chọn

--- src/main/resources/static/js/booking-select.js
+++ src/main/resources/static/js/booking-select.js
@@ -1,68 +1,89 @@
+$(document).on("click", ".custom-select-trigger", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
 
-$(document).on("click", ".custom-select-trigger", function(e) {
-    e.preventDefault();
-    e.stopPropagation();
+  // Tìm cái div cha có class .custom-select (nhưng không phải thẻ select)
+  const $visualSelect = $(this).closest("div.custom-select");
 
-    // Tìm cái div cha có class .custom-select (nhưng không phải thẻ select)
-    const $visualSelect = $(this).closest("div.custom-select");
+  // Đóng tất cả các cái khác
+  $("div.custom-select").not($visualSelect).removeClass("opened");
 
-    // Đóng tất cả các cái khác
-    $("div.custom-select").not($visualSelect).removeClass("opened");
-
-    // Toggle class cho cái div giả
-    $visualSelect.toggleClass("opened");
+  // Toggle class cho cái div giả
+  $visualSelect.toggleClass("opened");
 });
 
-$(document).on("click", ".custom-option", function(e) {
-    e.stopPropagation();
-    const $option = $(this);
-    const $customSelect = $option.closest(".custom-select");
-    const $wrapper = $option.closest(".custom-select-wrapper");
-    const val = $option.data("value");
-    const text = $option.text();
+$(document).on("click", ".custom-option", function (e) {
+  e.stopPropagation();
+  const $option = $(this);
+  const $customSelect = $option.closest(".custom-select");
+  const $wrapper = $option.closest(".custom-select-wrapper");
+  const val = $option.data("value");
+  const text = $option.text();
 
-    // 1. Cập nhật select thật (ẩn)
-    $wrapper.find("select").val(val).trigger('change');
+  // 1. Cập nhật select thật (ẩn)
+  $wrapper.find("select").val(val).trigger("change");
 
-    // 2. Cập nhật giao diện giả
-    $option.siblings().removeClass("selection");
-    $option.addClass("selection");
-    $customSelect.find(".custom-select-trigger").text(text);
-    $customSelect.removeClass("opened");
+  // 2. Cập nhật giao diện giả
+  $option.siblings().removeClass("selection");
+  $option.addClass("selection");
+  $customSelect.find(".custom-select-trigger").text(text);
+  $customSelect.removeClass("opened");
 });
 
 // Click ra ngoài thì đóng
-$(document).on("click", function() {
-    $(".custom-select").removeClass("opened");
+$(document).on("click", function () {
+  $(".custom-select").removeClass("opened");
 });
 
 function initCustomSelectForElement($element) {
-    $element.each(function() {
-        var $this = $(this);
-        if ($this.parent().hasClass("custom-select-wrapper")) return;
+  $element.each(function () {
+    var $this = $(this);
+    if ($this.parent().hasClass("custom-select-wrapper")) return;
 
-        var placeholder = $this.attr("placeholder") || "-- Chọn --";
+    var placeholder = $this.attr("placeholder") || "-- Chọn --";
 
-        // SỬA TẠI ĐÂY: Thêm 1 class định danh riêng cho div giả, ví dụ: 'custom-select-visual'
-        var template = '<div class="custom-select custom-select-visual">';
-        template += '<span class="custom-select-trigger">' + placeholder + '</span>';
-        template += '<div class="custom-options">';
+    // kiểm tra giá trị hiện tại
+    var currentText = placeholder;
+    var $selectedOption = $this.find("option:selected");
+    if ($selectedOption.length && $selectedOption.val() !== "") {
+      currentText = $selectedOption.text();
+    }
 
-        $this.find("option").each(function() {
-            var val = $(this).val();
-            if (val !== "") {
-                template += '<span class="custom-option" data-value="' + val + '">' + $(this).html() + '</span>';
-            }
-        });
-        template += '</div></div>';
+    // SỬA TẠI ĐÂY: Thêm 1 class định danh riêng cho div giả, ví dụ: 'custom-select-visual'
+    var template = '<div class="custom-select custom-select-visual">';
+    template +=
+      '<span class="custom-select-trigger">' + currentText + "</span>";
+    template += '<div class="custom-options">';
 
-        $this.wrap('<div class="custom-select-wrapper"></div>');
-        $this.hide(); // Ẩn cái select thật đi
-        $this.after(template);
+    $this.find("option").each(function () {
+      var val = $(this).val();
+      if (val !== "") {
+        var isSelected = $(this).is(":selected") ? " selection" : "";
+        template +=
+          '<span class="custom-option' +
+          isSelected +
+          '" data-value="' +
+          val +
+          '">' +
+          $(this).html() +
+          "</span>";
+        // template +=
+        //   '<span class="custom-option" data-value="' +
+        //   val +
+        //   '">' +
+        //   $(this).html() +
+        //   "</span>";
+      }
     });
+    template += "</div></div>";
+
+    $this.wrap('<div class="custom-select-wrapper"></div>');
+    $this.hide(); // Ẩn cái select thật đi
+    $this.after(template);
+  });
 }
 
-$(document).ready(function() {
-    // Gọi hàm cho tất cả các thẻ select có class .my-select
-    initCustomSelectForElement($("select.card-search-input"));
+$(document).ready(function () {
+  // Gọi hàm cho tất cả các thẻ select có class .my-select
+  initCustomSelectForElement($("select.card-search-input"));
 });
